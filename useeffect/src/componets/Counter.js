9134513831f0bbcfcbb1e4279cf9d8394815b234
MSJo <whalstn8@naver.com>
2021-03-02 23:04:21 +0900

    no message

--- useeffect/src/componets/Counter.js
+++ useeffect/src/componets/Counter.js
@@ -6,7 +6,7 @@ const Counter = (props) => {
   const increment = () => {
     setCount(count + 1);
   };
-  const clickString = props.click || "Click"; // props.click값 안에 값이 들어있으면 props.click쓰고 없으면 Click써라
+  const clickString = props.click || "Click"; // props.click값 안에 값이 들어있으면 props.click쓰고 없으면 'Click'써라
   return (
     <button onClick={increment}>
       {clickString}
