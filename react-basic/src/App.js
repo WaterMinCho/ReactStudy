ee074c6242eef598a6709a46e7a2db0cfc95c74e
MSJo <whalstn8@naver.com>
2021-03-02 20:36:54 +0900

    StudyFolder병합

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const[text, setText] = useState('Minsu');
+
+  const onSubmit = () =>{
+    alert('submitted');
+  }
+
+  const onKeyUp = (event) =>{
+    if (event.keyCode === 13){ //enter의 inegercode 13을 입력했을 때 onSubmit함수 실행하도록. 
+      //--> javascript 이벤트함수 사용법 숙지가 필요할 것으로 보임.
+      onSubmit();
+    }
+  }
+
+  // let text ='Minsu';
+
+  const updateText = () => {
+    // text ='genius';
+    setText('genius');
+    console.log(text);
+  }
+  
+  console.log(text);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp ={onKeyUp}/>
+      <button onClick={onSubmit}>Submit</button>
+
+      <br /> <br />
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
     </div>
   );
 }
